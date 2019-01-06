345943ec49e9c04eebe910ca9856772d76dc2261
Revathi Shanmugam <srevathikarthik@gmail.com>
2019-01-05 21:11:39 -0800

    Link component added

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
 
 class Home extends React.Component {
@@ -21,7 +22,7 @@ class Home extends React.Component {
           </div>
         </div>
         <div className="open-search">
-          <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>)
   }
