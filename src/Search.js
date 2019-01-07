c362e4f36cce2b5e0562b6f9bfdd2ee4db19cadb
Revathi Shanmugam <srevathikarthik@gmail.com>
2019-01-07 05:33:09 -0800

    Add book function created

--- src/Search.js
+++ src/Search.js
@@ -1,26 +1,69 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
 
 class Search extends React.Component {
+  state = {
+    Books: [],
+    query: ''
+  }
+  handleChange = (event) => {var value = event.target.value
+    this.setState(() => {
+      return {query: value}})
+    this.searchBooks(value)
+  }
+  changeBookShelf = (books) => {
+  let all_Books = this.props.myBooks
+  for (let book of books) {
+    book.shelf = "none"
+  }
+  for (let book of books) {
+    for (let b of all_Books) {
+      if (b.id === book.id) {
+        book.shelf = b.shelf
+      }
+    }
+  }
+  return books
+ }
+  searchBooks = (val)=> {
+    if(val.length !== 0) {
+      BooksAPI.search(val,10).then((books)=>{
+        if (books.length>0) {
+          this.setState(()=>{
+            return {Books:books.filter((book)=>(book.imageLinks))}
+          })
+        }
+      })
+    }
+    else
+    {
+      this.setState({
+        Books: [],
+        query: ''
+      })
+    }
+  }
+  addBook = (book, shelf) => {this.props.onChange(book,shelf)
+    this.props.onChange(book,shelf)}
   render(){
     return(
       <div className="search-books">
         <div className="search-books-bar">
-          <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+          <Link to='/' className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-
+            <input type="text" placeholder="Search by title or author"
+            value={this.state.query} onChange={this.handleChange}/>
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+          {this.state.query.length>0 && this.state.Books.map((book,index)=>
+            (<Book book={book} key={index} onUpdate={(shelf)=>
+              {this.addBook(book,shelf)}}/>)
+            )}
+          </ol>
         </div>
       </div>
     )
